8682b98c00622a7d842593634eba7f69d04d9481
Hemant Sharma <itshemantsharma173@gmail.com>
2023-10-22 11:59:09 +0530

    stylised comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text, Link } from "@chakra-ui/react";
 import { Avatar } from "@chakra-ui/react";
 
 const Comments = ({ author, text }) => {
@@ -6,15 +6,21 @@ const Comments = ({ author, text }) => {
     <Box py={4}>
       <Flex alignItems="start">
         <Avatar size="sm" name={author} mr={4} />
-        <Text fontSize="md" fontWeight="bold">
+        <Link
+          href={`https://news.ycombinator.com/user?id=${author}`}
+          fontSize="md"
+          fontWeight="bold"
+          isExternal
+        >
           {author}
-        </Text>
+        </Link>
       </Flex>
       <Text
         fontSize="md"
         color="gray.500"
         mt={2}
         dangerouslySetInnerHTML={{ __html: text }}
+        className="comment-text"
       />
     </Box>
   );

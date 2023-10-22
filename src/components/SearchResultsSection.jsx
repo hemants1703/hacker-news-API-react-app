73fdbcb24df9415b20ad91fc094a7b5e211830ef
Hemant Sharma <itshemantsharma173@gmail.com>
2023-10-22 10:26:55 +0530

    Updates

--- src/components/SearchResultsSection.jsx
+++ src/components/SearchResultsSection.jsx
@@ -4,7 +4,17 @@ import { Link } from "react-router-dom";
 
 import SearchResult from "./SearchResult";
 
+import { useEffect, useState } from "react";
+
 const SearchResultsSection = ({ searchResults }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (searchResults.length > 0) {
+      setIsLoading(false);
+    }
+  }, [searchResults]);
+
   return (
     <Box className="search-results-section">
       <Heading as="h2" size="sm" fontWeight={"light"}>
@@ -20,17 +30,21 @@ const SearchResultsSection = ({ searchResults }) => {
           display: searchResults.length === 0 ? "none" : "block",
         }}
       >
-        {searchResults.map((hits, index) => (
-          <Link to={`/post/${hits.objectID}`} key={index}>
-            <SearchResult
-              authorUsername={hits.author}
-              articleTitle={hits.title}
-              articleUrl={hits.url}
-              objectId={hits.objectID}
-            />
-            <Divider />
-          </Link>
-        ))}
+        {isLoading ? (
+          <Skeleton height="100px" />
+        ) : (
+          searchResults.map((hits, index) => (
+            <Link to={`/post/${hits.objectID}`} key={index}>
+              <SearchResult
+                authorUsername={hits.author}
+                articleTitle={hits.title}
+                articleUrl={hits.url}
+                objectId={hits.objectID}
+              />
+              <Divider />
+            </Link>
+          ))
+        )}
       </Box>
     </Box>
   );
